e632a8eb4b22dcde9211f5f17393b95d7cd6745b
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2020-03-02 16:00:53 -0500

    updates to seeds

--- data/seeds/04-instructions.js
+++ data/seeds/04-instructions.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("instructions")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("instructions").insert([
