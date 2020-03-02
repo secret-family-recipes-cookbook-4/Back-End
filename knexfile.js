91549c0bf8dcb862d7727963345ac9acf5063e09
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2020-03-02 13:52:11 -0500

    working to deploy

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATA_URL,
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
